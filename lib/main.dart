506ca357718cdacf97aefc070822168dd0b563e1
6631501032 <6631501032@lamduan.mfu.ac.th>
2025-09-11 16:52:44 +0700

    Add ramdon demo

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_couse/week05/design_2_demo.dart';
+import 'package:flutter_couse/week05/random_demo.dart';
 
 
 
 
 
 void main() {
-  runApp(MaterialApp(home: Design2Demo()));
+  runApp(MaterialApp(home: RandomDemo()));
 }
