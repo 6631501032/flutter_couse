e5e31cbd1da5a47cd77e538bc4c8dc67d482d1d1
6631501032 <6631501032@lamduan.mfu.ac.th>
2025-09-11 15:50:04 +0700

    Simple container design

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_couse/week05/multi_child_demo.dart';
+import 'package:flutter_couse/week05/design_1_demo.dart';
+
 
 
 
 
 void main() {
-  runApp(MaterialApp(home: MultiChildDemo()));
+  runApp(MaterialApp(home: Design1Demo()));
 }
