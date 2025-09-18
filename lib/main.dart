fb80f99f9ac43d3df450891c77bf56f1493a94d0
6631501032 <6631501032@lamduan.mfu.ac.th>
2025-09-18 16:21:33 +0700

    Add dynamic login demo

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_couse/week06/baking_app.dart';
+import 'package:flutter_couse/week06/login_dynamic.dart';
 
 void main() {
-  runApp(MaterialApp(home: BakingApp(),debugShowCheckedModeBanner: false,));
+  runApp(MaterialApp(home: LoginDynamic(),debugShowCheckedModeBanner: false,));
 }
