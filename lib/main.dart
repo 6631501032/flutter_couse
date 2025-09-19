2a21bb675be9ca85eb4a40b890352c2ee58ce068
6631501032 <6631501032@lamduan.mfu.ac.th>
2025-09-19 15:52:02 +0700

    Add Cooking app sing in

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_couse/week06/countdown.dart';
+import 'package:flutter_couse/cooking_sign_in.dart';
 
 void main() {
-  runApp(MaterialApp(home: Countdown(),debugShowCheckedModeBanner: false,));
+  runApp(MaterialApp(home: CookingSignIn(),debugShowCheckedModeBanner: false,));
 }
