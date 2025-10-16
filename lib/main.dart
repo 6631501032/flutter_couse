8de76675bbde94b4165bb0f59bd1ef8b99256548
6631501032 <6631501032@lamduan.mfu.ac.th>
2025-10-16 15:13:49 +0700

    Add Tab demo

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_couse/week08/scrollview_demo.dart';
+import 'package:flutter_couse/week08/tab_demo.dart';
 
 void main() {
-  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: ScrollviewDemo()));
+  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: TabDemo()));
 }
---
+++ lib/week08/tab_demo.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class TabDemo extends StatefulWidget {
+  const TabDemo({super.key});
+
+  @override
+  State<TabDemo> createState() => _TabDemoState();
+}
+
+class _TabDemoState extends State<TabDemo> {
+  String msg = 'Home';
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Tab demo'),
+          bottom: TabBar(
+            tabs: [
+              Tab(icon: Icon(Icons.home), text: msg),
+              Tab(icon: Icon(Icons.train), text: 'Train'),
+              Tab(icon: Icon(Icons.bike_scooter), text: 'Bike'),
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: [
+            Container(color: Colors.yellow, child: Text(msg)),
+            Container(color: Colors.red, child: Icon(Icons.abc_outlined)),
+            Container(
+              color: Colors.blue,
+              child: FilledButton(
+                onPressed: () {
+                  setState(() {
+                    msg = 'Changed!';
+                  });
+                },
+                child: Text('data'),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
