2e70680e60ee0111750efd08995c10fabe5524d6
6631501032 <6631501032@lamduan.mfu.ac.th>
2025-11-29 23:15:51 +0700

    Add snackbar demo

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'fishing6631501032.dart';
+import 'package:flutter_couse/week09/snackbar_demo.dart';
 
 void main() {
-  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: Fishing()));
+  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: SnackBarDemo()));
 }
---
+++ lib/week09/snackbar_demo.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+
+class SnackBarDemo extends StatelessWidget {
+  const SnackBarDemo({super.key});
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('SnackBar Demo'),
+      ),
+      body: FilledButton(
+        onPressed: () {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+              content: const Text('Hello Snackbar'),
+              duration: const Duration(seconds: 1), //default is 4 secs
+              action: SnackBarAction(
+                label: 'Undo',
+                onPressed: () {
+                  debugPrint('Undo something');
+                },
+              ),
+            ),
+          );
+
+        },
+        child: const Text('Show'),
+      ),
+    );
+  }
+}
+
